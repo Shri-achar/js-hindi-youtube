230a8d5f3e6643a236d65d0fc664b22d8edbe394
Shri-achar <76679845+Shri-achar@users.noreply.github.com>
2024-10-16 01:04:33 +0530

    datatypes_1

---
+++ .devcontainer/02_dataTypes.js
@@ -0,0 +1,15 @@
+let name="krishn"
+let age=14
+let isLoggedIn=false
+
+//////DATATypes
+//number=> 2 to power 53
+//bigint 
+//string=>""
+//boolean=>true/false
+//null =>standalone value
+//undefined=
+//symbol
+//object 
+
+console.log(typeof(age))
